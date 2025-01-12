7ccc50c12fb17441ff41dde1d83ffb0a04b2690d
Carlos9K <carlosashraf557@gmail.com>
2025-01-12 10:59:57 +0000

    Add new field to implement sorting of nodes by row_index

--- financial_report_builder/financial_report_builder/doctype/report_schema/report_schema_view.py
+++ financial_report_builder/financial_report_builder/doctype/report_schema/report_schema_view.py
@@ -3,22 +3,17 @@ import frappe
 def get_nodes():
     
     nodes = get_schema_children("Es-Bas Report Schema")
-
     node_data = []
-    report_source_list=[]
-    
+
     for node in nodes:
         doc = frappe.get_doc('Report Schema', node)
-        report_source = doc.report_source
-        report_source_list.append(report_source)
-
-        # Append the account list and report source as a tuple
         node_data.append(doc)
+    
+    node_data.sort(key=lambda x: x.row_index)
 
-    return node_data , report_source_list
+    return node_data
     
 def get_schema_children(parent_name):
-   
     parent_doc = frappe.get_doc('Report Schema', parent_name)
     
     # Use get_children to iterate over all child schemas
@@ -28,4 +23,3 @@ def get_schema_children(parent_name):
         children.extend(get_schema_children(child.name))
 
     return children
-
