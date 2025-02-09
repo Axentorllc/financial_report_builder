73d395c539b6d4ea19fede620bba1601e206542c
Kerolles Fathy <kerolles.f@outlook.com>
2025-02-09 13:32:31 +0200

    feat: migrate build row based on accounts  (#10)
    - Migrate build row based on accounts utils
    - Added report schema param to get nodes to make it dynamic
    - Init formula's rows that do not exist in eval locals to avoid eval errors

--- financial_report_builder/financial_report_builder/doctype/report_schema/report_schema_view.py
+++ financial_report_builder/financial_report_builder/doctype/report_schema/report_schema_view.py
@@ -1,27 +1,28 @@
 import frappe
 
-def get_nodes():
-    
-    nodes = get_schema_children("Es-Bas Report Schema")
+
+def get_nodes(report_schema_name: str):
+    nodes = get_schema_children(report_schema_name)
     node_data = []
-    report_sources=[]
+    report_sources = []
 
     for node in nodes:
-        doc = frappe.get_doc('Report Schema', node)
+        doc = frappe.get_doc("Report Schema", node)
         report_sources.append(doc.report_source)
         node_data.append(doc)
-    
+
     node_data.sort(key=lambda x: x.row_index)
 
-    return node_data ,set(report_sources)
-    
+    return node_data, set(report_sources)
+
+
 def get_schema_children(parent_name):
-    parent_doc = frappe.get_doc('Report Schema', parent_name)
-    
+    parent_doc = frappe.get_doc("Report Schema", parent_name)
+
     # Use get_children to iterate over all child schemas
     children = []
     for child in parent_doc.get_children():
-        children.append(child.name)  
+        children.append(child.name)
         children.extend(get_schema_children(child.name))
 
     return children
